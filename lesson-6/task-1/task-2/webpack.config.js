4f12329ee08fd7bbea325e5a2b34438e6e9f064b
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-05-29 18:12:22 +0300

    Add files via upload

--- lesson-6/task-1/task-2/webpack.config.js
+++ lesson-6/task-1/task-2/webpack.config.js
@@ -1,5 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -8,6 +10,10 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        test: /.js$/,
+        use: ['babel-loader'],
+      },
       {
         test: /.s?css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
@@ -28,6 +34,8 @@ module.exports = {
     ],
   },
   plugins: [
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
