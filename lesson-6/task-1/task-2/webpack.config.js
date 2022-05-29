d5a99253dc86553c66762a5ac3631270ac2b8c7d
DimaIsProgramm <93841542+DimaIsProgramm@users.noreply.github.com>
2022-05-29 14:43:46 +0300

    Add files via upload

--- lesson-6/task-1/task-2/webpack.config.js
+++ lesson-6/task-1/task-2/webpack.config.js
@@ -9,6 +9,23 @@ module.exports = {
         test: /.js$/,
         use: ['babel-loader'],
       },
+      {
+        test: /.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /.(jpg|png)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: '[name].[ext]',
+              outputPath: 'images',
+            },
+          },
+        ],
+      },
     ],
   },
 };
